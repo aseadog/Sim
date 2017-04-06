ee168aa31e07a875bec1b1c7150ff7c5ca0f333f
Seadog <andrewmarkseaman@gmail.com>
2017-04-06 18:44:23 +0100

    Add project files.

---
+++ Sim/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace Sim
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.LargeChange = 10;
+            this.trackBar1.Location = new System.Drawing.Point(100, 550);
+            this.trackBar1.Maximum = 450;
+            this.trackBar1.Minimum = 300;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(164, 45);
+            this.trackBar1.TabIndex = 0;
+            this.trackBar1.Value = 380;
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(350, 550);
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(104, 45);
+            this.trackBar2.TabIndex = 1;
+            this.trackBar2.Value = 100;
+            this.trackBar2.Maximum = 120;
+            this.trackBar2.Minimum = 60;
+            this.trackBar2.LargeChange = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(829, 368);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+    }
+}
+
---
+++ Sim/Form1.cs
@@ -0,0 +1,389 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sim
+{
+    public partial class Form1 : Form
+    {
+
+        List<PictureBox> droplets = new List<PictureBox>();
+        const int nDroplets = 200;
+        Size sizeDroplet = new Size(2,2);
+        Random rand = new Random();
+        const int screenWidth = 1000;
+        const int screenHeight = 600;
+        Color dropletColor = Color.Blue;
+        Timer simTime;
+        const int simTimeInterval = 1;
+        int[,] speed = new int[nDroplets, 2];
+        const double gravity = 1;
+        double errorMargin = 3;
+        double errornext = 5;
+        Color sidesColor = Color.Black;
+        double dampner = 0.5;
+        PictureBox taxDivider = new PictureBox();
+        PictureBox taxDividerBottom = new PictureBox();
+        PictureBox taxBottom = new PictureBox();
+
+        double[,] loc = new double [nDroplets, 2];
+        double[,] vel = new double[nDroplets, 2];
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.DoubleBuffered = true;
+            this.Width = screenWidth;
+            this.Height = screenHeight;
+            this.CenterToScreen();
+            this.Controls.Add(taxDivider);
+            this.Controls.Add(taxDividerBottom);
+            this.Controls.Add(taxBottom);
+
+            for (int i = 0; i < nDroplets; i++)
+            {
+                
+                vel[i, 0] = rand.Next(1, 3);
+                vel[i, 1] = rand.Next(0, 3);
+
+            }
+
+          
+
+            for (int i = 0; i < nDroplets; i++)
+            {
+                PictureBox droplet = new PictureBox();
+                droplet.Size = sizeDroplet;
+                loc[i, 0] = rand.Next(150, 400);
+                loc[i, 1] = rand.Next(50, 100);
+                droplet.Location = new Point(Convert.ToInt32(loc[i,0]), Convert.ToInt32(loc[i, 1]));
+                droplet.BackColor = dropletColor;
+                droplets.Add(droplet);
+            }
+
+            foreach (PictureBox droplet in droplets)
+            {
+                this.Controls.Add(droplet);
+            }
+
+            simTime = new Timer();
+            simTime.Interval = simTimeInterval;
+            simTime.Enabled = true;
+            simTime.Tick += new EventHandler(simTimeTick);
+
+        }
+
+
+        void simTimeTick(object sender, EventArgs e)
+        {
+           
+
+            PictureBox incomeRight = new PictureBox();
+            incomeRight.Location = new Point(180, 100);
+            incomeRight.BackColor = sidesColor;
+            incomeRight.Size = new Size(30, 350);
+            this.Controls.Add(incomeRight);
+
+            
+            taxDivider.Location = new Point(trackBar1.Value, 120);
+            taxDivider.BackColor = sidesColor;
+            taxDivider.Size = new Size(20, 60);
+            
+            
+
+            PictureBox taxRight = new PictureBox();
+            taxRight.Location = new Point(300, 180);
+            taxRight.BackColor = sidesColor;
+            taxRight.Size = new Size(30, 80);
+            this.Controls.Add(taxRight);
+
+           
+            taxBottom.Location = new Point(200, 260);
+            taxBottom.BackColor = sidesColor;
+            taxBottom.Size = new Size(trackBar2.Value, 30);
+           
+
+
+            taxDividerBottom.Location = new Point(300, 160);
+            taxDividerBottom.BackColor = sidesColor;
+            taxDividerBottom.Size = new Size(taxDivider.Location.X - 300, 20);
+            
+
+
+
+            PictureBox mainBottom = new PictureBox();
+            mainBottom.Location = new Point(130, 500);
+            mainBottom.BackColor = sidesColor;
+            mainBottom.Size = new Size(400, 20);
+            this.Controls.Add(mainBottom);
+
+            PictureBox maintop = new PictureBox();
+            maintop.Location = new Point(100, 20);
+            maintop.BackColor = sidesColor;
+            maintop.Size = new Size(500, 30);
+            this.Controls.Add(maintop);
+
+            PictureBox incomeLeft = new PictureBox();
+            incomeLeft.Location = new Point(100, 20);
+            incomeLeft.BackColor = sidesColor;
+            incomeLeft.Size = new Size(30, 500);
+            this.Controls.Add(incomeLeft);
+
+            PictureBox mainsupplytop = new PictureBox();
+            mainsupplytop.Location = new Point(180, 80);
+            mainsupplytop.BackColor = sidesColor;
+            mainsupplytop.Size = new Size(150, 30);
+            this.Controls.Add(mainsupplytop);
+
+
+            for (int i = 0; i < nDroplets; i++)
+            {
+
+                vel[i, 1] = vel[i, 1] + gravity;
+
+               
+                loc[i, 0] = loc[i, 0] + vel[i, 0];
+                loc[i, 1] = loc[i, 1] + vel[i, 1];
+               
+
+                if (checkCollisionXL(droplets[i]))
+                {
+                    vel[i, 0] = -vel[i, 0];
+                    loc[i, 0] = -loc[i, 0];
+                    
+                }
+
+             
+
+                if (checkCollisionXR(droplets[i]))
+                {
+                    vel[i, 0] = -vel[i, 0];
+                    loc[i, 0] = loc[i, 0] - 2 * (loc[i, 0] + Convert.ToDouble(sizeDroplet.Width) - Convert.ToDouble(screenWidth));
+                }
+
+                if (checkCollisionYT(droplets[i]))
+                {
+                    vel[i, 1] = -vel[i, 1];
+                    loc[i, 1] = -loc[i, 1];
+                }
+
+                if (checkCollisionYB(droplets[i]))
+                {
+                    vel[i, 1] = -vel[i, 1]*dampner;
+                    loc[i, 1] = loc[i, 1] - 2 * (loc[i, 1] + Convert.ToDouble(sizeDroplet.Height) - Convert.ToDouble(screenHeight));
+                }
+
+
+                //side collisions finished, still in for loop
+
+                // chamber collisions
+
+               
+
+                if (droplets[i].Location.X >= incomeRight.Location.X && droplets[i].Location.X < incomeRight.Location.X + errornext && droplets[i].Location.Y > incomeRight.Bounds.Top && droplets[i].Location.Y < incomeRight.Bounds.Bottom)
+                {
+                    vel[i, 0] = -vel[i, 0];
+                    loc[i, 0] = 2 * incomeRight.Location.X - loc[i, 0];
+                }
+
+                if (droplets[i].Location.X <= incomeRight.Bounds.Right && droplets[i].Location.X > incomeRight.Bounds.Right - errornext && droplets[i].Location.Y > incomeRight.Bounds.Top && droplets[i].Location.Y < incomeRight.Bounds.Bottom)
+                {
+                    vel[i, 0] = -vel[i, 0]*dampner;
+                    loc[i, 0] = loc[i, 0] + 2 * (incomeRight.Bounds.Right - loc[i,0]);
+                }
+
+
+                if (droplets[i].Location.X >= taxDivider.Bounds.Left && droplets[i].Location.X < taxDivider.Bounds.Left + errornext && droplets[i].Location.Y > taxDivider.Bounds.Top && droplets[i].Location.Y < taxDivider.Bounds.Bottom)
+                {
+                    vel[i, 0] = -vel[i, 0];
+                    loc[i, 0] = 2 * taxDivider.Location.X - loc[i, 0];
+                }
+
+                if (droplets[i].Location.Y + sizeDroplet.Height >= taxDivider.Bounds.Top && droplets[i].Location.Y  + sizeDroplet.Height< taxDivider.Bounds.Top + errornext && droplets[i].Location.X > taxDivider.Bounds.Left && droplets[i].Location.X < taxDivider.Bounds.Right)
+                {
+                    vel[i, 1] = -vel[i, 1];
+                    loc[i, 1] = loc[i, 1] - 2 * (loc[i, 1] + sizeDroplet.Height - taxDivider.Bounds.Top);
+                }
+
+                if (droplets[i].Location.X < taxDivider.Bounds.Right && droplets[i].Location.X > taxDivider.Bounds.Right - errornext && droplets[i].Location.Y > taxDivider.Bounds.Top && droplets[i].Location.Y < taxDivider.Bounds.Bottom)
+                {
+                    vel[i, 0] = -vel[i, 0];
+                    loc[i, 0] = loc[i, 0] + 2 * (taxDivider.Bounds.Right-loc[i,0]);
+                }
+
+                if (droplets[i].Location.X >= taxRight.Bounds.Left && droplets[i].Location.X < taxRight.Bounds.Left + errornext && droplets[i].Location.Y > taxRight.Bounds.Top && droplets[i].Location.Y < taxRight.Bounds.Bottom)
+                {
+                    vel[i, 0] = -vel[i, 0];
+                    loc[i, 0] = 2 * taxRight.Location.X - loc[i, 0];
+                }
+
+                if (droplets[i].Location.Y >= mainBottom.Location.Y && droplets[i].Location.Y < mainBottom.Bounds.Bottom && droplets[i].Location.X > mainBottom.Bounds.Left && droplets[i].Location.X < mainBottom.Bounds.Right)
+                {
+                    vel[i, 1] = -vel[i, 1]*dampner;
+                    loc[i, 1] = loc[i,1] - 2 * (loc[i, 1] + sizeDroplet.Height - mainBottom.Bounds.Top);
+
+                }
+
+                if (droplets[i].Location.Y >= taxBottom.Location.Y && droplets[i].Location.Y < taxBottom.Bounds.Bottom && droplets[i].Location.X > taxBottom.Bounds.Left && droplets[i].Location.X < taxBottom.Bounds.Right)
+                {
+                    vel[i, 1] = -vel[i, 1] * dampner;
+                    loc[i, 1] = loc[i, 1] - 2 * (loc[i, 1] + sizeDroplet.Height - taxBottom.Bounds.Top);
+
+                }
+
+                if (droplets[i].Location.Y >= taxDividerBottom.Location.Y && droplets[i].Location.Y < taxDividerBottom.Bounds.Bottom && droplets[i].Location.X > taxDividerBottom.Bounds.Left && droplets[i].Location.X < taxDividerBottom.Bounds.Right)
+                {
+                    vel[i, 1] = -vel[i, 1] * dampner;
+                    loc[i, 1] = loc[i, 1] - 2 * (loc[i, 1] + sizeDroplet.Height - taxDividerBottom.Bounds.Top);
+
+                }
+
+                if (droplets[i].Location.Y >= mainsupplytop.Location.Y && droplets[i].Location.Y < mainsupplytop.Bounds.Bottom && droplets[i].Location.X > mainsupplytop.Bounds.Left && droplets[i].Location.X < mainsupplytop.Bounds.Right)
+                {
+                    vel[i, 1] = -vel[i, 1] * dampner;
+                    loc[i, 1] = loc[i, 1] - 2 * (loc[i, 1] + sizeDroplet.Height - mainsupplytop.Bounds.Top);
+
+                }
+
+
+                if (droplets[i].Location.X <= incomeLeft.Bounds.Right && droplets[i].Location.X > incomeLeft.Bounds.Right - errornext && droplets[i].Location.Y > incomeLeft.Bounds.Top && droplets[i].Location.Y < incomeLeft.Bounds.Bottom)
+                {
+                    vel[i, 0] = -vel[i, 0];
+                    loc[i, 0] = loc[i, 0] + 2 * (incomeLeft.Bounds.Right - loc[i, 0]);
+                }
+
+
+                if (droplets[i].Location.X >= incomeLeft.Location.X && droplets[i].Location.X < incomeLeft.Location.X + errornext && droplets[i].Location.Y > incomeLeft.Bounds.Top && droplets[i].Location.Y < incomeLeft.Bounds.Bottom)
+                {
+                    vel[i, 0] = -vel[i, 0];
+                    loc[i, 0] = 2 * incomeLeft.Location.X - loc[i, 0];
+                }
+
+                //top
+
+                if (droplets[i].Location.Y <= maintop.Bounds.Bottom && droplets[i].Location.Y > maintop.Bounds.Bottom - errornext && droplets[i].Location.X >= maintop.Bounds.Left && droplets[i].Location.X < maintop.Bounds.Right - sizeDroplet.Width)
+                {
+                    vel[i, 1] = -vel[i, 1] * dampner;
+                    loc[i, 1] = loc[i, 1] + 2 * (maintop.Bounds.Bottom - loc[i,1]);
+
+                }
+
+                // income pump
+
+                if (droplets[i].Location.X > incomeLeft.Bounds.Right && droplets[i].Location.X < incomeRight.Bounds.Left )
+                {
+                    vel[i, 0] = 0;
+                    vel[i, 1] = -5;
+                }
+
+                if (droplets[i].Location.X > incomeLeft.Bounds.Right && droplets[i].Location.X < incomeRight.Bounds.Left && droplets[i].Bounds.Bottom< mainsupplytop.Bounds.Top)
+                {
+                    vel[i, 0] = 3;
+                    
+                }
+
+
+                // check droplet collisions
+
+                for (int j = 0; j < nDroplets; j++) // check x collision
+                {
+                    if (loc[i,0] + Convert.ToDouble(sizeDroplet.Width) > loc[j,0] - errorMargin && loc[i, 0] + Convert.ToDouble(sizeDroplet.Width) < loc[j, 0] + errorMargin && loc[i,1] + Convert.ToDouble(sizeDroplet.Height) >= loc[j,1] && loc[i,1] <= loc[j,1] + Convert.ToDouble(sizeDroplet.Height) && i!=j)
+                   
+
+                    {
+                        double hold;
+                        //loc[i, 0] = loc[i, 0] - vel[j, 0];
+                        //loc[j, 0] = loc[j, 0] - vel[i, 0];
+                        hold = vel[i, 0];
+                        vel[i, 0] = vel[j, 0];
+                        vel[j, 0] = hold;
+
+                        //vel[i, 0] = 0;  // Test collision
+                        //vel[j, 0] = 0;
+                        //vel[i, 1] = 0;
+                        //vel[j, 1] = 0;
+                    }
+
+                }
+
+                for (int j = 0; j < nDroplets; j++)  // check y collision
+                {
+                    if (loc[i, 1] + Convert.ToDouble(sizeDroplet.Height) > loc[j, 1] - errorMargin && loc[i, 1] + Convert.ToDouble(sizeDroplet.Height) < loc[j, 1] + errorMargin && loc[i, 0] + Convert.ToDouble(sizeDroplet.Width) >= loc[j, 0] && loc[i, 0] <= loc[j, 0] + Convert.ToDouble(sizeDroplet.Width) && i != j)
+                    {
+                        double hold;
+                        //loc[i, 1] = loc[i, 1] - vel[j, 1];
+                        //loc[j, 1] = loc[j, 1] - vel[i, 1];
+                        hold = vel[i, 1];
+                        vel[i, 1] = vel[j, 1];
+                        vel[j, 1] = hold;
+
+                        //vel[i, 0] = 0;    // To test collision
+                        //vel[j, 0] = 0;
+                        //vel[i, 1] = 0;
+                        //vel[j, 1] = 0;
+
+                    }
+
+                }
+
+                droplets[i].Location = new Point(Convert.ToInt32(loc[i, 0]), Convert.ToInt32(loc[i, 1]));
+                
+
+            }
+
+            
+                      
+
+        }
+
+
+        public bool checkCollisionXL(PictureBox inputPictureBox)
+        {
+            if (inputPictureBox.Bounds.X <= 0 )
+                return true;
+            return false;
+        }
+
+        public bool checkCollisionXR(PictureBox inputPictureBox)
+        {
+            if ( inputPictureBox.Bounds.Right >= screenWidth)
+                return true;
+            return false;
+        }
+
+        public bool checkCollisionYT(PictureBox inputPictureBox)
+        {
+            if (inputPictureBox.Bounds.Top <= 0)
+                return true;
+            return false;
+        }
+
+        public bool checkCollisionYB(PictureBox inputPictureBox)
+        {
+            if ( inputPictureBox.Bounds.Bottom >= screenHeight)
+                return true;
+            return false;
+        }
+
+        public bool checkCollisionLeftMain(PictureBox inputPictureBox, PictureBox inputBoundary)
+        {
+            if (inputPictureBox.Bounds.IntersectsWith(inputBoundary.Bounds))
+                return true;
+            return false;
+        }
+
+
+
+
+        
+
+       
+    }
+}
